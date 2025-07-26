b244ce478b4d42e0840b918bfcb651c32c1630e7
Maria Pia Zaniboni <mpiazaniboni@gmail.com>
2025-07-26 13:35:34 -0300

    avances

--- src/managers/CartManager.js
+++ src/managers/CartManager.js
@@ -1,13 +1,12 @@
-
-import { error } from 'console';
 import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 class CartManager {
     constructor(filePath){
-        this.filePath = filePath || path.join(__dirname, '../data/carts.json'); 
+        this.filePath = path.resolve(__dirname, '..', __filename);
     }
 
     async saveCarts(){
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,32 +1,40 @@
 import { Router } from "express";
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 import ProductManager from "../managers/ProductManager.js";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const router = Router();
 
-const manager = new ProductManager( path.join(__dirname, '../data/products.json') );
+const productManager = new ProductManager(  './src/data/product.json' );
 
 
 router.get('/', async(req, res) => {
-    try {
-        const products = await manager.getProducts();
-        res.json(products);
-    } catch {
-        res.status(500).json({ error: 'Error getting products'});
-    }
+    const products = await productManager.getProducts();
+    products ? res.json(products) : res.status(500).json({ error: 'Error getting products'});
 });
 
 router.get('/:pid', async (req, res) => {
-    try {
-        const id = parseInt(req.params.pid);
-        const product = await manager.getProductById(id);
-        res.json(product);
-    } catch (err) {
-        res.status(404).json({ error: err.message });
-    }
+    const pid = parseInt(req.params.pid);
+    const product = await productManager.getProductById(id);
+
+    product ? res.json(product) : res.status(404).json({ error: err.message });
+});
+
+//Post
+router.post('/', async (req, res) => {
+    const newProduct = await productManager.addProduct(req.body);
+
+    res.status(201).json(newProduct);
+});
+
+//update
+router.put('/:pid', async (req, res) => {
+    const updated = await productManager.updateProduct(req.params.pid, req.body);
+    updated ? res.json(updated) : res.status(404).json({ error: err.message });
+});
+
+//delete
+router.delete('/:pid', async (req, res) => {
+    const result = await productManager.deleteProduct(req.params.pid);
+    res.json(result);
 });
 
 export default router;
